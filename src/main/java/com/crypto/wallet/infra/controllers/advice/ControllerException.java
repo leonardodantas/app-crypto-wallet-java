ef7b54dff3a8a671b12c518c63c2ed53d9d757cc
leonardo <leonardo1317@yahoo.com.br>
2022-01-18 08:01:16 -0300

    Tratamento de erro do validation bean

--- src/main/java/com/crypto/wallet/infra/controllers/advice/ControllerException.java
+++ src/main/java/com/crypto/wallet/infra/controllers/advice/ControllerException.java
@@ -3,13 +3,27 @@ package com.crypto.wallet.infra.controllers.advice;
 import com.crypto.wallet.app.exceptions.CryptocurrencyNotFoundException;
 import com.crypto.wallet.app.exceptions.EntitySaveException;
 import com.crypto.wallet.infra.controllers.advice.response.ErrorDTO;
+import com.crypto.wallet.infra.controllers.advice.response.ErrorsDTO;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ControllerException {
+
+    private final MessageSource messageSource;
+
+    public ControllerException(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @ExceptionHandler(value = CryptocurrencyNotFoundException.class)
         public ResponseEntity<?> coinNotFoundException(CryptocurrencyNotFoundException error) {
@@ -21,5 +35,16 @@ public class ControllerException {
     public ResponseEntity<?> entitySaveException(EntitySaveException error) {
         ErrorDTO errorDTO = ErrorDTO.from(error.getMessage());
         return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> validationBeanException(MethodArgumentNotValidException error) {
+        List<FieldError> fields = error.getBindingResult().getFieldErrors();
+
+        List<ErrorsDTO> errors = fields.stream()
+                .map(field -> ErrorsDTO.of(field, messageSource.getMessage(field, LocaleContextHolder.getLocale())))
+                .collect(Collectors.toUnmodifiableList());
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/com/crypto/wallet/infra/controllers/advice/response/ErrorsDTO.java
@@ -0,0 +1,24 @@
+package com.crypto.wallet.infra.controllers.advice.response;
+
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+
+import java.util.UUID;
+
+@Getter
+public class ErrorsDTO {
+
+    private final String id;
+    private final String field;
+    private final String message;
+
+    private ErrorsDTO(FieldError field, String message) {
+        this.id = UUID.randomUUID().toString();
+        this.field = field.getField();
+        this.message = message;
+    }
+
+    public static ErrorsDTO of(FieldError field, String message) {
+        return new ErrorsDTO(field, message);
+    }
+}
