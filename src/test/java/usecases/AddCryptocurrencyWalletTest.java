5259010a77eed18768eb93a0ec2bb6626cf6c698
leonardo dantas <leonardo1317@yahoo.com.br>
2024-08-13 19:45:00 -0300

    tests: Adicionando testes unitarios

---
+++ src/test/java/usecases/AddCryptocurrencyWalletTest.java
@@ -0,0 +1,69 @@
+package usecases;
+
+
+import com.crypto.wallet.app.exceptions.CryptocurrencyNotFoundException;
+import com.crypto.wallet.app.models.requests.CryptocurrencyWalletRequest;
+import com.crypto.wallet.app.repositories.IDigitalCurrencyAcronymRepository;
+import com.crypto.wallet.app.usecases.impl.AddCryptocurrencyWallet;
+import com.crypto.wallet.app.usecases.impl.SaveWallet;
+import com.crypto.wallet.domain.DigitalCurrencyAcronym;
+import com.crypto.wallet.domain.Wallet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import utils.GetMockJson;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AddCryptocurrencyWalletTest {
+
+    @InjectMocks
+    private AddCryptocurrencyWallet cryptocurrencyWallet;
+    @Mock
+    private IDigitalCurrencyAcronymRepository digitalCurrencyAcronymRepository;
+    @Mock
+    private SaveWallet saveWallet;
+
+    @Test
+    void shouldThrownCryptocurrencyNotFoundException() {
+        final var request = GetMockJson.execute("requests/cryptocurrency-wallet-invalid", CryptocurrencyWalletRequest.class);
+
+        when(digitalCurrencyAcronymRepository.findByName(anyString()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(CryptocurrencyNotFoundException.class, () -> cryptocurrencyWallet.addCryptocurrency(request));
+
+        verify(saveWallet, never()).save(any(), any());
+    }
+
+    @Test
+    void shouldSaveCryptocurrencyInWallet() {
+        final var cryptocurrencyWalletRequest = GetMockJson.execute("requests/cryptocurrency-wallet-valid", CryptocurrencyWalletRequest.class);
+
+        final var digitalCurrencyAcronym = GetMockJson.execute("entities/digital-currency-acronym", DigitalCurrencyAcronym.class);
+        final var wallet = GetMockJson.execute("entities/wallet", Wallet.class);
+
+        when(digitalCurrencyAcronymRepository.findByName(anyString()))
+                .thenReturn(Optional.of(digitalCurrencyAcronym));
+
+        when(saveWallet.save(any(), any()))
+                .thenReturn(wallet);
+
+        final var result = cryptocurrencyWallet.addCryptocurrency(cryptocurrencyWalletRequest);
+
+        assertNotNull(result);
+        assertEquals(wallet.getId(), result.getId());
+        assertEquals(wallet.getQuantity(), result.getQuantity());
+
+        verify(saveWallet).save(any(), any());
+    }
+
+}
---
+++ src/test/java/usecases/SaveWalletTest.java
@@ -0,0 +1,87 @@
+
+package usecases;
+
+import com.crypto.wallet.app.models.requests.CryptocurrencyWalletRequest;
+import com.crypto.wallet.app.repositories.ISalesHistoryRepository;
+import com.crypto.wallet.app.repositories.IWalletRepository;
+import com.crypto.wallet.app.usecases.impl.SaveWallet;
+import com.crypto.wallet.domain.DigitalCurrencyAcronym;
+import com.crypto.wallet.domain.Wallet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import utils.GetMockJson;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SaveWalletTest {
+
+    @InjectMocks
+    private SaveWallet saveWallet;
+    @Mock
+    private ISalesHistoryRepository salesHistoryRepository;
+    @Mock
+    private IWalletRepository walletRepository;
+    @Captor
+    private ArgumentCaptor<Wallet> argumentCaptorWallet;
+
+    @Test
+    void shouldUpdateCryptoInWallet() {
+        final var cryptocurrencyWalletRequest = GetMockJson.execute("requests/cryptocurrency-wallet-valid", CryptocurrencyWalletRequest.class);
+        final var digitalCurrencyAcronym = GetMockJson.execute("entities/digital-currency-acronym", DigitalCurrencyAcronym.class);
+        final var walletToSave = GetMockJson.execute("entities/wallet-1", Wallet.class);
+        final var walletSave = GetMockJson.execute("entities/wallet", Wallet.class);
+
+        when(walletRepository
+                .findByDigitalCurrencyAcronym(any())).thenReturn(Optional.of(walletSave));
+
+        when(walletRepository
+                .save(any())).thenReturn(walletToSave);
+
+        final var result = saveWallet.save(cryptocurrencyWalletRequest, digitalCurrencyAcronym);
+
+        assertNotNull(result);
+        assertEquals(20, result.getQuantity());
+
+        verify(salesHistoryRepository).save(any());
+        verify(walletRepository).save(argumentCaptorWallet.capture());
+
+        final var walletSaveExpected = argumentCaptorWallet.getValue();
+        assertEquals(20, walletSaveExpected.getQuantity());
+    }
+
+    @Test
+    void shouldSaveWalletWhenNotFoundCriptoInDataBase() {
+        final var cryptocurrencyWalletRequest = GetMockJson.execute("requests/cryptocurrency-wallet-valid", CryptocurrencyWalletRequest.class);
+        final var digitalCurrencyAcronym = GetMockJson.execute("entities/digital-currency-acronym", DigitalCurrencyAcronym.class);
+        final var walletToSave = GetMockJson.execute("entities/wallet", Wallet.class);
+
+        when(walletRepository
+                .findByDigitalCurrencyAcronym(any())).thenReturn(Optional.empty());
+
+        when(walletRepository
+                .save(any())).thenReturn(walletToSave);
+
+        final var result = saveWallet.save(cryptocurrencyWalletRequest, digitalCurrencyAcronym);
+
+        assertNotNull(result);
+        assertEquals(10, result.getQuantity());
+
+        verify(salesHistoryRepository).save(any());
+        verify(walletRepository).save(argumentCaptorWallet.capture());
+
+        final var walletSaveExpected = argumentCaptorWallet.getValue();
+        assertEquals(10, walletSaveExpected.getQuantity());
+    }
+}
