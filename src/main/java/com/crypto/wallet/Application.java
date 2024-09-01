a8f77fcce84ed240861478c972b374cae8195174
leonardo dantas <leonardo1317@yahoo.com.br>
2024-09-01 10:30:37 -0300

    feat: Habilitando schedule

--- src/main/java/com/crypto/wallet/Application.java
+++ src/main/java/com/crypto/wallet/Application.java
@@ -3,9 +3,11 @@ package com.crypto.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
 @EnableFeignClients
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
